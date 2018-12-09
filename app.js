935d96929c39fa3ef27e706ec6e538771f15b9ef
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-12-09 11:35:17 -0500

    added mongodb and mongoose with starting info in db

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
-var campgrounds = [
-  { name: "Topeekeegee Lake", image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-  { name: "Salmon Creek", image: "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" },
-  { name: "Salt Dunes", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }
-];
+// newurlparser used because of deprecating url error, mongo version is greater than 3.1.1
+mongoose.connect('mongodb://localhost/yelp-camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+// MAKING RELATION BETWEEN SCHEMA AND MODEL
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//   name: "Salmon Creek",
+//   image: "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+// }, (err, campground) => {
+//   if (err) {
+//     console.log("Error")
+//   } else {
+//     console.log('NEWLY CREATED CAMPGROUND');
+//   }
+// });
 
 app.get('/', (req, res) => {
   res.render('landing')
 });
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds: campgrounds });
+  // GET ALL CAMPGROUNDS FROM DB
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('campgrounds', { campgrounds: campgrounds });
+    }
+  });
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -23,9 +46,14 @@ app.post('/campgrounds', (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image }
-  campgrounds.push(newCampground)
-  // redirect back to campgrounds route
-  res.redirect('/campgrounds');
+  // CREATE A NEW CAMPGROUND AND SAVE TO DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get('/campgrounds/new', (req, res) => {
