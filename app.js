66d4f6dab0af19b7f4936c45e5a04b3b50462c3d
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-12-19 21:37:12 -0500

    styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const Campground = require('./models/campground');
+const Comment = require('./models/comment')
 const seedDB = require('./seeds');
 
 seedDB();
@@ -70,7 +71,37 @@ app.get('/campgrounds/:id', (req, res) => {
 });
 
 app.get('/campgrounds/:id/comments/new', (req, res) => {
-  res.render('comments/new')
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', { campground: campground });
+    }
+  });
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+  // lookup cg by id
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds')
+    } else {
+      // we can pass in comment because in the form we nested text and name within comment comment[text], and comment[author]
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err)
+        } else {
+          campground.comments.push(comment);
+          campground.save(campground);
+          res.redirect('/campgrounds/' + campground._id)
+        }
+      })
+    }
+  });
+  // create new comment
+  // connect new comment to campground
+  // redirect back to cg show page
 });
 
 app.listen(3000, () => {
