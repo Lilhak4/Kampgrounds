bc89e10f40fe897e7fca8f558756a53bb2907d41
Kahlil Gersbach <kahlilgersbach@gmail.com>
2019-01-07 18:55:11 -0500

    changed comment model to make a user comment association

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const commentSchema = mongoose.Schema({
   text: String,
-  author: String
+  author: {
+    id: {
+      type: mongoose.Schema.Tyoes.ObjectId,
+      ref: "User"
+    },
+    username: String
+  }
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
