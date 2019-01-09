fe6a5d801d813a154c828efc6ac8563d0f8ae5ff
Kahlil Gersbach <kahlilgersbach@gmail.com>
2019-01-08 20:57:54 -0500

    authorization for editing campgrounds is implemented

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -50,17 +50,41 @@ router.get('/:id', (req, res) => {
 });
 
 // EDIT CAMPGROUND
-router.get('/:id/edit', (req, res) => {
-  Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      console.log(err)
-      res.redirect('/campgrounds')
-    } else {
-      res.render('campgrounds/edit', { campground: foundCampground });
-    }
-  });
+// router.get('/:id/edit', (req, res) => {
+//   Campground.findById(req.params.id, (err, foundCampground) => {
+//     if (err) {
+//       console.log(err)
+//       res.redirect('/campgrounds')
+//     } else {
+//       res.render('campgrounds/edit', { campground: foundCampground });
+//     }
+//   });
+// });
 
+router.get('/:id/edit', (req, res) => {
+  // is user logged in?
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+      if (err) {
+        console.log(err)
+        res.redirect('/campgrounds')
+      } else {
+        // does user own campground?
+        // method of mongoose that returns useable id 
+        if (foundCampground.author.id.equals(req.user._id)) {
+          res.render('campgrounds/edit', { campground: foundCampground });
+        } else {
+          res.send('YOU DO NOT HAVE PERMISSION TO DO THAT');
+        }
+      }
+    });
+  } else {
+    console.log('YOU NEED TO BE LOGGED IN TO DO THAT');
+    res.send('YOU NEED TO BE LOGGED IN TO DO THAT');
+  }
+  // if not, redirect
 });
+
 // UPDATE CAMPGROUND
 router.put('/:id', (req, res) => {
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
