c9b205176654c6a6d62ec6a65eab4debd15f0a17
Kahlil Gersbach <kahlilgersbach@gmail.com>
2019-01-09 17:19:50 -0500

    delete route now implemented

--- routes/comments.js
+++ routes/comments.js
@@ -66,7 +66,14 @@ router.put('/:comment_id', (req, res) => {
 
 // COMMENTS DESTROY
 router.delete('/:comment_id', (req, res) => {
-  res.send('this is the destroy route');
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+      console.log(err);
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  })
 });
 
 // MIDDLEWARE
