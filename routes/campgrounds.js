75a7a9828fd1a82621d4987a5b2aef38f52d06f9
Kahlil Gersbach <kahlilgersbach@gmail.com>
2019-01-10 20:10:48 -0500

    middleware completely fixed

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const middleware = require('../middleware');
 const Campground = require('../models/campground')
 
 router.get('/', (req, res) => {
